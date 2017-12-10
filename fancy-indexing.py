ed3f5c7f8ca8f5beef11f8448dac91dbb3f3b22b
Ben <benjamin.phillips22@gmail.com>
2017-12-10 12:05:09 +1100

    and a histogram

--- fancy-indexing.py
+++ fancy-indexing.py
@@ -3,6 +3,7 @@
 # %%
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 # %%
 
@@ -16,4 +17,27 @@ print(x[1::2])
 
 # %%
 
+# the shape of the result reflects the shape
+# of the index arrays rather than the shape
+# of the array being indexed
 
+x = np.arange(10)
+y = np.array([[1, 2], [2, 3]])
+
+print(x[y])
+
+# %%
+
+x = np.arange(10)
+y = [0, 1, 2, 3, 9]
+print(x)
+x[y] += 1
+print(x)
+
+# %%
+
+np.random.seed(42)
+x = np.random.randn(100)
+bins = np.linspace(-5, 5, 20)
+
+plt.hist(x, bins, histtype='step')
