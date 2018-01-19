800cbeb8795e4052b594d2bec700b1e1a479bd9c
ISELECT1\ben.phillips <benjamin.phillips22@gmail.com>
2018-01-19 13:57:07 +1100

    finished

--- structured-data.py
+++ structured-data.py
@@ -8,12 +8,22 @@ import numpy as np
 # compound data for structured arrays
 
 data = np.zeros(3, dtype={
-    'names':('name', 'age', 'weight'),
-    'formats':('U10', 'i4', 'f8')
+    'names': ('name', 'age', 'weight'),
+    'formats': ('U10', 'i4', 'f8')
 })
 
 print(data)
 
 data['name'] = ['Alice', 'Bob', 'Cathy']
+data['age'] = [100, 99, 98]
+data['weight'] = [10.3, 55.3, 100/3]
 
 print(data)
+
+# %%
+
+# get all names
+print(data['name'])
+
+# get all of Alice's details
+print(data[0])
