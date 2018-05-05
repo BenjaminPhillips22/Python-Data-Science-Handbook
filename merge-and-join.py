0343da47822fd01196a02d599bcee5533917f53d
Ben <benjamin.phillips22@gmail.com>
2018-05-05 10:54:55 +1000

    a little merge

---
+++ merge-and-join.py
@@ -0,0 +1,23 @@
+# %%
+
+import numpy as np
+import pandas as pd
+
+# %%
+
+
+def make_df(cols, ind):
+    """Quickly make a DataFrame"""
+    data = {c: [str(c) + str(i) for i in ind]
+            for c in cols}
+    return pd.DataFrame(data, ind)
+
+
+# %%
+
+df1 = make_df('ABC', range(3))
+df2 = make_df('ADE', range(3))
+
+# %%
+
+print(pd.merge(df1, df2, ))
